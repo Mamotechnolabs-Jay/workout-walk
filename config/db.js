2f9c9f6efa9da8335a95c277a2c9ce3799e50b45
Mamotechnolabs-zahid <zahid@mamotechnolabs.com>
2025-08-06 18:32:50 +0530

    Remove sensitive information from .env file

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
-
+ 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected');
+ 
+    console.log(`MongoDB Connected`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error('Database connection error:', error.message);
     process.exit(1);
   }
 };
-
+ 
 module.exports = connectDB;
